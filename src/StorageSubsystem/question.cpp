c766bc9adcc7eca201bff580b6b75280c00d1e9b
captokie <captokie@users.noreply.github.com>
2015-12-05 12:41:50 -0500

    updated question object

--- src/StorageSubsystem/question.cpp
+++ src/StorageSubsystem/question.cpp
@@ -1,28 +1,7 @@
 #include "question.h"
 
-Question::Question(int id, QString* personal, QString* desired, QString* category)
-    : id(id), personal(personal), desired(desired), category(category)
+Question::Question()
 { }
 
 Question::~Question()
 { }
-
-int Question::getId() const
-{
-    return id;
-}
-
-const QString& Question::getPersonal() const
-{
-    return *personal;
-}
-
-const QString& Question::getDesired() const
-{
-    return *desired;
-}
-
-const QString& Question::getCategory() const
-{
-    return *category;
-}
--- src/StorageSubsystem/question.h
+++ src/StorageSubsystem/question.h
@@ -9,24 +9,17 @@
 class Question
 {
     public:
-        Question(int id, QString* personal, QString* desired, QString* category);
+        Question();
         virtual ~Question();
 
-    protected:
-        int id;
-        QString* personal;
-        QString* desired;
-        QString* category;
-
     public:
-        int getId() const;
-        const QString& getPersonal() const;
-        const QString& getDesired() const;
-        const QString& getCategory() const;
+        virtual int getId() const = 0;
+        virtual const QString& getText() const = 0;
+        virtual const QString& getCategory() const = 0;
 
         virtual const QMap<int, Answer*>& getAnswers() const = 0;
-        virtual const Answer& getPersonalAnswer() const = 0;
-        virtual const Answer& getDesiredAnswer() const = 0;
+        virtual const Answer& getPersonal() const = 0;
+        virtual const Answer& getDesired() const = 0;
 
         virtual void setPersonal(const Answer& answer) = 0;
         virtual void setDesired(const Answer& answer) = 0;
